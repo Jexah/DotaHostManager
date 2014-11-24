1a95848ea01c28feb1c7d34e19783e7e49a12671
ash47 <ash.fourtyseven@gmail.com>
2014-11-24 18:09:56 +1100

    It will now download and extract steamcmd if it doesnt already exist

--- DotaHostBoxManager/Program.cs
+++ DotaHostBoxManager/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
 
 namespace DotaHostBoxManager
@@ -17,6 +18,9 @@ namespace DotaHostBoxManager
         // The steam cmd file to run steam commands with
         static readonly String STEAMCMD = "steamcmd.exe";
 
+        // The zip containing steamcmd
+        static readonly String STEAMCMD_ZIP = "steamcmd.zip";
+
         // The path to download steamcmd from
         static readonly String STEAMCMD_DOWNLOAD_PATH = "http://media.steampowered.com/installer/steamcmd.zip";
 
@@ -26,16 +30,35 @@ namespace DotaHostBoxManager
         // The main entry point into the program
         static void Main(string[] args)
         {
-            // Check if steamcmd exists
-            if (File.Exists(g_BASEPATH+STEAMCMD))
-            {
-                log("It exists");
-            }
-            else
-            {
-                log("We need to download steamcmd.");
-            }
+            // Delete the old log file
+            File.Delete(g_BASEPATH + "log.txt");
 
+            // Ensure steamcmd exists
+            verifySteamcmd();
+
+
+        }
+
+        // This function ensures steamcmd is available
+        static void verifySteamcmd()
+        {
+            // Check if steamcmd exists
+            if (!File.Exists(g_BASEPATH + STEAMCMD))
+            {
+                // If there is an old version of steamcmd.zip, delete it
+                File.Delete(g_BASEPATH + STEAMCMD_ZIP);
+
+                // NOTE: WE NEED TO CATCH EXCEPTIONS HERE INCASE STEAM UNREACHABLE!
+
+                // Download steamcmd zip
+                dlManager.DownloadFile(STEAMCMD_DOWNLOAD_PATH, STEAMCMD_ZIP);
+
+                // Extract the archive
+                ZipFile.ExtractToDirectory(STEAMCMD_ZIP, g_BASEPATH);
+
+                // Delete the zip
+                File.Delete(g_BASEPATH + STEAMCMD_ZIP);
+            }
         }
 
         // Outputs to the console and stores a copy into log.txt
