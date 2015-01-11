9fd6a24e24835b9c0605511b7ae53b5d64d658c7
= <ijexah@gmail.com>
2015-01-12 05:35:28 +1100

    List for OpenStackServer object, also contains some helper functions. Fixed #11

---
+++ DotaHostLibrary/Types/OpenStackServerList.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace DotaHostLibrary
+{
+    public class OpenStackServerList
+    {
+        public int Count
+        {
+            get
+            {
+                return servers.Count;
+            }
+        }
+
+        public List<OpenStackServer> servers;
+
+        public string[] getAllIpsAsArray()
+        {
+            string[] ips = new string[servers.Count];
+            forEach((server, i) =>
+            {
+                ips[i] = server.Ip;
+                return false;
+            });
+            return ips;
+        }
+        public string getAllIpsAsString()
+        {
+            string[] ips = new string[servers.Count];
+            forEach((server, i) =>
+            {
+                ips[i] = server.Ip;
+                return false;
+            });
+            return String.Join("\n", ips);
+        }
+
+        public void forEach(Func<OpenStackServer, int, bool> func)
+        {
+            for (int i = 0; i < servers.Count; ++i)
+            {
+                if (func(servers[i], i))
+                {
+                    break;
+                }
+            }
+        }
+
+        public OpenStackServerList(dynamic d)
+        {
+            servers = new List<OpenStackServer>();
+            for (int i = 0; i < d["servers"].Count; ++i)
+            {
+                servers.Add(new OpenStackServer(d["servers"][i]));
+            }
+        }
+    }
+}
