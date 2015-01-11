01a2807210a27e0c739820ffb53543b6ccd0e7b7
= <ijexah@gmail.com>
2015-01-12 05:35:00 +1100

    Server object for OpenStack api/server/detail call.

---
+++ DotaHostLibrary/Types/OpenStackServer.cs
@@ -0,0 +1,53 @@
+﻿
+namespace DotaHostLibrary
+{
+    public class OpenStackServer
+    {
+        public dynamic Dynamic { get; set; }
+        public string Ip
+        {
+            get
+            {
+                return Dynamic["addresses"]["Ext-Net"][0]["addr"];
+            }
+            set
+            {
+                Dynamic["addresses"]["Ext-Net"][0]["addr"] = value;
+            }
+        }
+        public string FlavorID
+        {
+            get
+            {
+                return Dynamic["flavor"]["id"];
+            }
+            set
+            {
+                Dynamic["flavor"]["id"] = value;
+            }
+        }
+        public string InstanceID
+        {
+            get
+            {
+                return Dynamic["id"];
+            }
+            set
+            {
+                Dynamic["id"] = value;
+            }
+        }
+        public string Region
+        {
+            get
+            {
+                return FlavorID == Runabove.LARGE_SANDBOX_FRANCE ? Runabove.FRANCE : Runabove.CANADA;
+            }
+        }
+
+        public OpenStackServer(dynamic d)
+        {
+            Dynamic = d;
+        }
+    }
+}
