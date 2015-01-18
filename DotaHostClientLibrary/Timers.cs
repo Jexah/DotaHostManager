ed40ca917e15cbaf40292c6e39719df06e6e2153
= <ijexah@gmail.com>
2015-01-18 15:34:13 +1100

    Added Timers.SetInterval

--- DotaHostClientLibrary/Timers.cs
+++ DotaHostClientLibrary/Timers.cs
@@ -53,5 +53,41 @@ namespace DotaHostClientLibrary
             return timer.Dispose;
         }
 
+
+        public static EndTimer SetInterval(int duration, byte type, TimerCallback func)
+        {
+            int properDuration;
+            switch (type)
+            {
+                case Seconds:
+                    properDuration = duration * 1000;
+                    break;
+                case Minutes:
+                    properDuration = duration * 1000 * 60;
+                    break;
+                case Hours:
+                    properDuration = duration * 1000 * 60 * 60;
+                    break;
+                default:
+                    properDuration = duration;
+                    break;
+            }
+            // Creates a new timer with the given duration
+            var timer = new Timer(properDuration);
+
+            // Sets the elapsed function
+            timer.Elapsed += (sender, e) =>
+            {
+                // Call the function given from the function argument
+                func();
+            };
+
+            // Enables the timer
+            timer.Enabled = true;
+
+            // Returns dispose function, call this to delete the timer.
+            return timer.Dispose;
+        }
+
     }
 }
